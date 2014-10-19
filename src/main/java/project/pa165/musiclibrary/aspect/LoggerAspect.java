a1ebb16eb8940fb4c71cf18fe2f3d439d09ab3d5
Milan <milan.panik1@gmail.com>
2014-10-19 13:48:13 +0200

    slf4j bridged to log4j instead of just using log4j alone

--- src/main/java/project/pa165/musiclibrary/aspect/LoggerAspect.java
+++ src/main/java/project/pa165/musiclibrary/aspect/LoggerAspect.java
@@ -1,9 +1,12 @@
 package project.pa165.musiclibrary.aspect;
 
-import org.apache.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
 
 /**
  * Aspect to log all execution of methods in project.pa165.musiclibrary package,
@@ -18,15 +21,15 @@ import org.aspectj.lang.annotation.Aspect;
 @Aspect
 public class LoggerAspect {
 
-    private final Logger LOG = Logger.getLogger(LoggerAspect.class);
+    private final Logger LOG = LoggerFactory.getLogger(LoggerAspect.class);
 
     @Around(value = "execution(* project.pa165.musiclibrary..*(..)) )")
-    public Object onMethodExecution(ProceedingJoinPoint joinPoint){
+    public Object onMethodExecution(ProceedingJoinPoint joinPoint) {
         Object result = null;
         try {
             result = joinPoint.proceed();
         } catch (Throwable throwable) {
-            LOG.fatal(joinPoint.getSignature().getName() + " " + joinPoint.getArgs(), throwable);
+            //LOG.fatal(joinPoint.getSignature().getName() + " " + Arrays.toString(joinPoint.getArgs()), throwable);
         }
         logMessage(joinPoint.getSignature().getName(), joinPoint.getArgs(), result);
         return result;
@@ -34,11 +37,7 @@ public class LoggerAspect {
 
     private void logMessage(String methodName, Object[] methodArguments, Object result) {
         StringBuilder messageBuilder = new StringBuilder(methodName).append("(");
-
-        for (Object obj : methodArguments) {
-            messageBuilder.append(obj);
-        }
-
+        messageBuilder.append(Arrays.toString(methodArguments));
         messageBuilder.append("): ").append(result);
         LOG.info(messageBuilder.toString());
     }
