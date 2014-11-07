c817cb5d9ab05e9fc08fe56b1d68a4339c828673
Martin Zahuta <martinzahuta@gmail.com>
2014-11-07 12:44:14 +0100

    UserManagerImplTest

--- service/src/main/java/project/pa165/musiclibrary/dto/UserDto.java
+++ service/src/main/java/project/pa165/musiclibrary/dto/UserDto.java
@@ -19,24 +19,48 @@ public class UserDto {
     public Long getId() {
         return id;
     }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getEmail() {
         return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getFirstName() {
         return firstName;
     }
+    
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
     public String getLastName() {
         return lastName;
+    }
+    
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getPassword() {
         return password;
     }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String getRole() {
         return role;
+    }
+    
+    public void setRole(String role) {
+        this.role = role;
     }
 }
