e10133eae00ee5117975c5993f0d1bdb3f991841
MatusBurda <burda.matus@gmail.com>
2014-12-15 22:44:53 +0100

    AlbumController update
    added create/delete/update/getAll methods

--- web/src/main/java/project/pa165/musiclibrary/rest/AlbumController.java
+++ web/src/main/java/project/pa165/musiclibrary/rest/AlbumController.java
@@ -3,7 +3,9 @@ package project.pa165.musiclibrary.rest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import project.pa165.musiclibrary.dto.AlbumDto;
+import project.pa165.musiclibrary.entities.Album;
 import project.pa165.musiclibrary.exception.AlbumNotFoundException;
+import project.pa165.musiclibrary.exception.ServiceException;
 import project.pa165.musiclibrary.services.AlbumService;
 import project.pa165.musiclibrary.util.ErrorInfo;
 
@@ -27,23 +29,43 @@ public class AlbumController {
     public void setAlbumService(AlbumService albumService) {
         this.albumService = albumService;
     }
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public void getAllAlbums() throws ServiceException{
+        albumService.getAllAlbums();
+    }
 
     @RequestMapping(value = "/search", method = RequestMethod.GET)
-    public List<AlbumDto> getAlbumsByTerm(@RequestParam("term") String term) {
+    public List<AlbumDto> getAlbumsByTerm(@RequestParam("term") String term) throws ServiceException {
         return getAlbumService().findAlbumByTitle(term);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public AlbumDto getAlbumById(@PathVariable("id") Long id) throws AlbumNotFoundException {
+    public AlbumDto getAlbumById(@PathVariable("id") Long id) throws ServiceException, AlbumNotFoundException {
         AlbumDto album = getAlbumService().findAlbum(id);
-        if (album == null) throw new AlbumNotFoundException(id.toString());
+        if (album == null)
+            throw new AlbumNotFoundException(id.toString());
         return album;
+    }
+    
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deleteAlbum(@PathVariable("id") Long id) throws ServiceException{
+        albumService.deleteAlbum(id);
+    }
+    
+    @RequestMapping(method = RequestMethod.POST)
+    public void createAlbum(@RequestBody AlbumDto albumDto) throws ServiceException{
+        albumService.createAlbum(albumDto);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public void updateSong(@RequestBody AlbumDto albumDto) throws ServiceException{
+        albumService.updateAlbum(albumDto);
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(AlbumNotFoundException.class)
-    public
-    @ResponseBody
+    public @ResponseBody
     ErrorInfo handleAlbumNotFoundException() {
         return new ErrorInfo(404, "Album could not be found");
     }
